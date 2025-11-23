4ba467ef5c46f9d98d52ea66664acdb3154a32a1
thimbunhuv <bunhov123@gmail.com>
2025-11-23 21:42:53 +0700

    update

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+async def create_item(name: str):
+    return {"name": name, "message": "Item created successfully"}
